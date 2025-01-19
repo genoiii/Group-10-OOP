54b37fb672df7105626017c1bf1158c4b05e5728
genoiii <152836107+genoiii@users.noreply.github.com>
2025-01-19 18:11:28 +0800

    Build a Login Page for MotorPh Payroll App

---
+++ MotorPhPayrollApp/src/motorphpayrollapp/MotorPhPayrollApp.java
@@ -0,0 +1,26 @@
+package motorphpayrollapp;
+
+
+import Frame.LoginPage;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author 63909
+ */
+public class MotorPhPayrollApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LoginPage loginPage = new LoginPage();
+        loginPage.setVisible(true);
+    }
+    
+}
